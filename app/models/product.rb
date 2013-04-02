c89812329a0e7c60f2f67a5088b46b30b1d3d011
r3zn1k <oliver.streuli@bluewin.ch>
2013-04-02 12:39:24 +0200

    Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,13 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
-end
+  
+  validates_presence_of :title, :description
+  validates :image_url, :format => { 
+	:with => %r{\.(gif|jpg|png)$}i, 
+	:message => 'must be a URL for GIF, JPG or PNG image.'
+  }
+  validates :price, :numericality => {					
+	:greater_than_or_equal_to => 0.01
+  }
+
+  end
