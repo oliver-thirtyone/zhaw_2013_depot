bcc6c89c46755fcf7bde898dfa6bf3687fa7c1b7
r3zn1k <oliver.streuli@bluewin.ch>
2013-04-07 15:12:47 +0200

    store

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,11 +3,11 @@ class Product < ActiveRecord::Base
   
   validates_presence_of :title, :description
   validates :image_url, :format => { 
-	:with => %r{\.(gif|jpg|png)$}i, 
-	:message => 'must be a URL for GIF, JPG or PNG image.'
+    :with => %r{\.(gif|jpg|png)$}i,
+    :message => 'must be a URL for GIF, JPG or PNG image.'
   }
-  validates :price, :numericality => {					
-	:greater_than_or_equal_to => 0.01
+  validates :price, :numericality => {
+    :greater_than_or_equal_to => 0.01
   }
 
   end
