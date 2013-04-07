5e941bb7a0252b2e50d5423bbb7ad14207ae3b83
r3zn1k <oliver.streuli@bluewin.ch>
2013-04-07 19:46:02 +0200

    layout

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,18 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
-  
-  validates_presence_of :title, :description
-  validates :image_url, :format => { 
-    :with => %r{\.(gif|jpg|png)$}i,
-    :message => 'must be a URL for GIF, JPG or PNG image.'
-  }
-  validates :price, :numericality => {
-    :greater_than_or_equal_to => 0.01
+
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_tahn_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+     with: %r{\.(gif|jpg|png)\Z}i,
+     message: 'must be a URL for GIF, JPG or PNG image.'
   }
 
+  def validate_business_rules
+     return unless self.meets_criteria
+     return unless self.validatation_applicable
+
+     errors.add(:fields, "cannot be set since criterias are not met")
   end
+end
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+  end
 end
